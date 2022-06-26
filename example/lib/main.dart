8dacd361be1810b6e7ecd2710632dfae221739e2
Tejaswi D Prakash <tejaswi@vigaet.com>
2022-06-26 16:24:56 +0530

    working, list selection for glb files is pending

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -9,18 +9,111 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Model Viewer")),
-        body: ModelViewer(
-          backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
-          //src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
-          src: 'etc/assets/Astronaut.glb', // a bundled asset file
-          alt: "A 3D model of an astronaut",
-          ar: true,
-          autoRotate: true,
-          cameraControls: true,
-        ),
-      ),
+      title: 'GLB viewer',
+      home: new MyHomePage()
     );
   }
 }
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key})  : super(key: key);
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _value = 'AnimatedMorphCube.glb';
+  ModelViewer glbViewer ;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new DropdownButton<String>(
+            value: _value,
+            items: <DropdownMenuItem<String>>[
+              new DropdownMenuItem(
+                child: new Text('Astronaut.glb'),
+                value: 'Astronaut.glb',
+              ),
+              new DropdownMenuItem(
+                  child: new Text('AnimatedMorphCube.glb'),
+                  value: 'AnimatedMorphCube.glb'
+              ),
+            ],
+            onChanged: (String value) {
+              print(value);
+              buttonPressed();
+              setState(() {
+                _value = value;
+
+                glbViewer = new ModelViewer(
+                backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+                //src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
+                src: 'etc/assets/'+_value,
+                // a bundled asset file
+                alt: "A 3D GLB model",
+                ar: true,
+                autoRotate: true,
+                cameraControls: true,
+                autoPlay: true
+
+                );
+              });
+            },)
+      ),
+      body:     Container(
+          margin: const EdgeInsets.all(10.0),
+          color: Colors.amber[600],
+
+          child: glbViewer
+
+
+
+    ),);
+
+  }
+Widget buttonPressed(){     return new Scaffold(
+  appBar: new AppBar(
+      title: new DropdownButton<String>(
+        value: _value,
+        items: <DropdownMenuItem<String>>[
+          new DropdownMenuItem(
+            child: new Text('Astronaut.glb'),
+            value: 'Astronaut.glb',
+          ),
+          new DropdownMenuItem(
+              child: new Text('AnimatedMorphCube.glb'),
+              value: 'AnimatedMorphCube.glb'
+          ),
+        ],
+        onChanged: (String value) {
+          print(value);
+          buttonPressed();
+          setState(() {
+            _value = value;
+
+            glbViewer = new ModelViewer(
+                backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+                //src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
+                src: 'etc/assets/'+_value,
+                // a bundled asset file
+                alt: "A 3D GLB model",
+                ar: true,
+                autoRotate: true,
+                cameraControls: true,
+                autoPlay: true
+
+            );
+          });
+        },)
+  ),
+  body:     Container(
+      margin: const EdgeInsets.all(10.0),
+      color: Colors.amber[600],
+
+      child: glbViewer
+
+
+
+  ),);
+}
+  }
